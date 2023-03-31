9736f685956a35d8dd0ec8c42a1b4309e6dabe0b
ysmur <ysmur@yandex.ru>
2023-03-31 19:15:35 +0300

    Задача Бот-помощник для игр (по кнопке two_dices реализован таймер на 10 сек)

--- buttons.py
+++ buttons.py
@@ -17,39 +17,62 @@ from dotenv import load_dotenv
 load_dotenv()
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 
-# Напишем соответствующие функции.
+base_keyboard = [['/dice', '/timer']]
+base_markup = ReplyKeyboardMarkup(base_keyboard, one_time_keyboard=False)
+
+dice_keyboard = [['/one_dice', '/two_dices'],
+                      ['/one_20_dice', '/back']]
+dice_markup = ReplyKeyboardMarkup(dice_keyboard, one_time_keyboard=False)
+
+closes_keyboard = [['/close']]
+closes_markup = ReplyKeyboardMarkup(closes_keyboard, one_time_keyboard=False)
 
 async def start(update, context):
-    reply_keyboard = [['/dice', '/timer']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text(
         "Я бот-помощник для настольных игр",
-        reply_markup=markup
+        reply_markup=base_markup
     )
 
 async def help(update, context):
     await update.message.reply_text(
         "Я бот-помощник для настольных игр.")
 
-
+# управление кубиком
 async def dice(update, context):
-    reply_keyboard = [['/one_dice', '/two_dices'],
-                      ['/one_20_dice', '/back']]
-    markup = ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=False)
     await update.message.reply_text(
         "Выбирай",
-        reply_markup=markup
+        reply_markup=dice_markup
     )
 
 async def one_dice(update, context):
     num = random.randint(1, 6)
     await update.message.reply_text(num)
 
+# управление таймером
+async def set_timer(update, context, time):
+    chat_id = update.effective_message.chat_id
+
+    job = context.job_queue.run_once(task,
+                                     time,
+                                     chat_id=chat_id,
+                                     name=str(chat_id),
+                                     data=time)
+    context.chat_data['job'] = job
+
+
+async def task(context):
+    time = context.chat_data['job'].data
+    print(time)
+    await context.bot.send_message(context.job.chat_id,
+                                   text=f'{time} cek',
+                                   reply_markup=dice_markup)
+
 
 async def timer(update, context):
-    await update.message.reply_text("Телефон: +7(495)776-3030")
-
-
+    text = 'Установить таймер на 10 сек'
+    await set_timer(update, context, 10)
+    await update.message.reply_text(text,
+                                    reply_markup=closes_markup)
 
 
 
@@ -64,7 +87,7 @@ def main():
     application = Application.builder().token(BOT_TOKEN).build()
     application.add_handler(CommandHandler("dice", dice))
     application.add_handler(CommandHandler("one_dice", one_dice))
-
+    application.add_handler(CommandHandler("two_dices", timer))
     application.add_handler(CommandHandler("back", start))
     application.add_handler(CommandHandler("timer", timer))
     application.add_handler(CommandHandler("help", help))
